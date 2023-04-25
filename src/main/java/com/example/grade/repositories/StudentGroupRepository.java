4e459c45b123a080d69a4af51cbbbbeac6df6146
NfolZordo <79935610+NfolZordo@users.noreply.github.com>
2023-04-25 14:41:33 +0300

    цщкл

--- src/main/java/com/example/grade/repositories/StudentGroupRepository.java
+++ src/main/java/com/example/grade/repositories/StudentGroupRepository.java
@@ -1,5 +1,6 @@
 package com.example.grade.repositories;
 
+import com.example.grade.models.CoursesForGroup;
 import com.example.grade.models.StudentGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,5 +11,8 @@ public interface StudentGroupRepository extends JpaRepository<StudentGroup, Inte
     @Query("select c.studentGroup from CoursesForGroup c where c.teacher.email = ?1 and c.studentGroup.active = true")
     List<StudentGroup> findGroupByTeacherEmail(String email);
     StudentGroup findAllById(Integer id);
+
+    @Query("select c from CoursesForGroup c where c.teacher.email = ?1 and c.course.semester = ?2 and c.studentGroup.active = true")
+    List<CoursesForGroup> findCourseByTeacherEmail(String email, Integer semester);
 
 }
