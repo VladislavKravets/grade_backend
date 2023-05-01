a7af4a242719db7282c2b8a003d8c61c4fac9d3b
NfolZordo <Nfolzordo@gmail.com>
2023-05-01 22:13:11 +0300

    Update TeacherRepository.java

--- src/main/java/com/example/grade/repositories/TeacherRepository.java
+++ src/main/java/com/example/grade/repositories/TeacherRepository.java
@@ -1,19 +1,17 @@
 package com.example.grade.repositories;
 
-import com.example.grade.models.StudentGroup;
 import com.example.grade.models.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
     Optional<Teacher> findByEmail(String email);
 
 
-    @Query(value="     SELECT 'student' AS table_name" +
+    @Query(value="SELECT 'student' AS table_name" +
             "    FROM student" +
             "    WHERE email = :email" +
             "    UNION ALL" +
